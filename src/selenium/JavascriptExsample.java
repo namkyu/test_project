2f80811bb3bd44480715d3fe3c6af588a263d03e
namkyu <lng1982@gmail.com>
2013-09-30 10:51:45 +0900

    selenium google chrome alert test

---
+++ src/selenium/JavascriptExsample.java
@@ -0,0 +1,37 @@
+package selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * @FileName : JavascriptExsample.java
+ * @Project : test_project
+ * @Date : 2013. 9. 30.
+ * @작성자 : nklee
+ * @프로그램설명 :
+ */
+public class JavascriptExsample {
+
+	@Test
+	public void alertTest() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("window-size=1024,768");
+
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		WebDriver driver = new ChromeDriver(capabilities);
+
+		driver.get("http://google.com/");
+
+		if (driver instanceof JavascriptExecutor) {
+			((JavascriptExecutor) driver).executeScript("alert('hello world');");
+		}
+	}
+}
