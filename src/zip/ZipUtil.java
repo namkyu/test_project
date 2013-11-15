79a47a323e62d2e6b8852af41bec33b66e2bb554
namkyu <lng1982@gmail.com>
2013-11-15 19:33:16 +0900

    Zip Util

--- src/zip/ZipUtil.java
+++ src/zip/ZipUtil.java
@@ -1,201 +1,287 @@
 package zip;
+
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.charset.Charset;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 
-/**
- * @FileName : ZipUtil.java
- * @Project : test_project
- * @Date : 2013. 10. 15.
- * @작성자 : nklee
- * @프로그램설명 :
- */
 public class ZipUtil {
 
-private static boolean debug = true;
+	/** 압축률 Default : 9 */
+	private static final int COMPRESSION_LEVEL = 8;
 
-	public static void main(String[] args) throws IOException {
-		new ZipUtil().zip(new File("E:\\test\\zip\\test_project"));
-	}
+	/** Buffer Size */
+	private static final int BUFFER_SIZE = 1024 * 2;
 
-	public void unzip(File zippedFile) throws IOException {
-		unzip(zippedFile, Charset.defaultCharset().name());
-	}
-
-	public void unzip(File zippedFile, String charsetName ) throws IOException {
-		unzip(zippedFile, zippedFile.getParentFile(), charsetName);
-	}
-	public void unzip(File zippedFile, File destDir) throws IOException {
-		unzip(new FileInputStream(zippedFile), destDir, Charset.defaultCharset().name());
+	public static void main(String[] args) throws Exception {
+//		ZipUtil.zip("E:\\test\\zip\\test_project", "E:\\test\\zip\\test_project.zip");
+//		ZipUtil.unzip("E:\\test\\zip\\test_project.zip", "E:\\test\\zip\\test_project");
 	}
 
-	public void unzip(File zippedFile, File destDir, String charsetName)
-	throws IOException {
-		unzip(new FileInputStream(zippedFile), destDir, charsetName);
+	/**
+	 * 압축 해제 압축 해제된 파일 목록을 List 반환
+	 *
+	 * @param zipFilePath
+	 * @param targetDir
+	 * @return
+	 * @throws Exception
+	 */
+	public List<String> unzipResultFileList(String zipFilePath, String targetDir, boolean isResultFileFullPath) throws Exception {
+		return unzipResultFileList(new File(zipFilePath), new File(targetDir), isResultFileFullPath);
 	}
-	public void unzip(InputStream is, File destDir) throws IOException{
-		unzip(is, destDir, Charset.defaultCharset().name());
-	}
-	public void unzip( InputStream is, File destDir, String charsetName)
-	throws IOException {
-		ZipArchiveInputStream zis ;
-		ZipArchiveEntry entry ;
-		String name ;
-		File target ;
-		int nWritten = 0;
-		BufferedOutputStream bos ;
-		byte [] buf = new byte[1024 * 8];
 
-		zis = new ZipArchiveInputStream(is, charsetName, false);
-		while ( (entry = zis.getNextZipEntry()) != null ){
-			name = entry.getName();
-			target = new File (destDir, name);
-			if ( entry.isDirectory() ){
-				target.mkdirs(); /*  does it always work? */
-				debug ("dir  : " + name);
-			} else {
-				target.createNewFile();
-				bos = new BufferedOutputStream(new FileOutputStream(target));
-				while ((nWritten = zis.read(buf)) >= 0 ){
-					bos.write(buf, 0, nWritten);
+	/**
+	 * 압축 해제 압축 해제된 파일 목록을 List 반환
+	 *
+	 * @param zipFile
+	 * @param targetDir
+	 * @param isResultFileFullPath
+	 * @return
+	 * @throws Exception
+	 */
+	public static List<String> unzipResultFileList(File zipFile, File targetDir, boolean isResultFileFullPath) throws Exception {
+		return unzipResultFileList(new FileInputStream(zipFile), targetDir, isResultFileFullPath);
+	}
+
+	/**
+	 * 압축 해제 압축 해제된 파일 목록을 List 반환
+	 *
+	 * @param inputStream
+	 * @param targetDir
+	 * @param isResultFileFullPath
+	 * @return
+	 * @throws Exception
+	 */
+	public static List<String> unzipResultFileList(InputStream inputStream, String targetDir, boolean isResultFileFullPath) throws Exception {
+		return unzipResultFileList(inputStream, new File(targetDir), isResultFileFullPath);
+	}
+
+	/**
+	 * 압축 해제 압축 해제된 파일 목록을 List 반환
+	 *
+	 * @param zipFile
+	 * @param targetDir
+	 * @return
+	 * @throws Exception
+	 */
+	public static List<String> unzipResultFileList(InputStream inputStream, File targetDir, boolean isResultFileFullPath) throws Exception {
+		List<String> unzipFileList = new ArrayList<String>();
+
+		ZipArchiveInputStream zi = null;
+		ZipArchiveEntry entry = null;
+		try {
+			zi = new ZipArchiveInputStream(inputStream, null, true);
+			while ((entry = zi.getNextZipEntry()) != null) {
+				String fileNameToUnzip = new String(entry.getRawName(), Charset.defaultCharset());
+				File targetFile = new File(targetDir, fileNameToUnzip);
+
+				if (entry.isDirectory()) {// Directory
+					FileUtils.forceMkdir(targetFile.getAbsoluteFile());
+				} else { // File
+					FileUtils.forceMkdir(targetFile.getParentFile());
+					unzipEntry(zi, targetFile);
+					if (isResultFileFullPath) {
+						unzipFileList.add(targetFile.getAbsolutePath());
+					} else {
+						unzipFileList.add(targetFile.getName());
+					}
 				}
-				bos.close();
-				debug ("file : " + name);
+			}
+		} finally {
+			if (zi != null) {
+				zi.close();
 			}
 		}
-		zis.close();
+
+		return unzipFileList;
 	}
 
 	/**
-	 * compresses the given file(or dir) and creates new file under the same directory.
-	 * @param src file or directory
-	 * @throws IOException
+	 * unzip Zip 파일 압축 해제
+	 *
+	 * @param zipFilePath
+	 *            - zip file
+	 * @param targetDir
+	 *            - 압축 해제 디렉토리
+	 * @throws Exception
 	 */
-	public void zip(File src) throws IOException{
-		zip(src, Charset.defaultCharset().name(), true);
+	public static void unzip(String zipFilePath, String targetDir) throws Exception {
+		unzip(new File(zipFilePath), new File(targetDir));
 	}
+
 	/**
-	 * zips the given file(or dir) and create
-	 * @param src file or directory to compress
-	 * @param includeSrc if true and src is directory, then src is not included in the compression. if false, src is included.
-	 * @throws IOException
+	 * unzip Zip 파일 압축 해제
+	 *
+	 * @param zipFile
+	 *            - zip file
+	 * @param targetDir
+	 *            - 압축 해제 디렉토리
+	 * @throws Exception
 	 */
-	public void zip(File src, boolean includeSrc) throws IOException{
-		zip(src, Charset.defaultCharset().name(), includeSrc);
+	public static void unzip(File zipFile, File targetDir) throws Exception {
+		unzip(new FileInputStream(zipFile), targetDir);
 	}
+
 	/**
-	 * compresses the given src file (or directory) with the given encoding
-	 * @param src
-	 * @param charSetName
-	 * @param includeSrc
-	 * @throws IOException
+	 * unzip Zip 파일 압축 해제
+	 *
+	 * @param zipFile
+	 *            - zip file
+	 * @param targetDir
+	 *            - 압축 해제 디렉토리
+	 * @throws Exception
 	 */
-	public void zip(File src, String charSetName, boolean includeSrc) throws IOException {
-		zip( src, src.getParentFile(), charSetName, includeSrc);
-	}
-	/**
-	 * compresses the given src file(or directory) and writes to the given output stream.
-	 * @param src
-	 * @param os
-	 * @throws IOException
-	 */
-	public void zip(File src, OutputStream os) throws IOException {
-		zip(src, os, Charset.defaultCharset().name(), true);
-	}
-	/**
-	 * compresses the given src file(or directory) and create the compressed file under the given destDir.
-	 * @param src
-	 * @param destDir
-	 * @param charSetName
-	 * @param includeSrc
-	 * @throws IOException
-	 */
-	public void zip(File src, File destDir, String charSetName, boolean includeSrc) throws IOException {
-		String fileName = src.getName();
-		if ( !src.isDirectory() ){
-			int pos = fileName.lastIndexOf(".");
-			if ( pos >  0){
-				fileName = fileName.substring(0, pos);
+	public static void unzip(InputStream inputStream, File targetDir) throws Exception {
+		ZipArchiveInputStream zi = null;
+		ZipArchiveEntry entry = null;
+		try {
+			zi = new ZipArchiveInputStream(inputStream, null, true);
+			while ((entry = zi.getNextZipEntry()) != null) {
+				String fileNameToUnzip = new String(entry.getRawName(), "EUC-KR");
+				File targetFile = new File(targetDir, fileNameToUnzip);
+
+				if (entry.isDirectory()) {// Directory
+					FileUtils.forceMkdir(targetFile.getAbsoluteFile());
+				} else { // File
+					FileUtils.forceMkdir(targetFile.getParentFile());
+					unzipEntry(zi, targetFile);
+				}
+			}
+		} finally {
+			if (zi != null) {
+				zi.close();
 			}
 		}
-		fileName += ".zip";
-
-		File zippedFile = new File ( destDir, fileName);
-		if ( !zippedFile.exists() ) zippedFile.createNewFile();
-		zip(src, new FileOutputStream(zippedFile), charSetName, includeSrc);
 	}
-	public void zip(File src, OutputStream os, String charsetName, boolean includeSrc)
-	throws IOException {
-		ZipArchiveOutputStream zos = new ZipArchiveOutputStream(os);
-		zos.setEncoding(charsetName);
-		FileInputStream fis ;
 
-		int length ;
-		ZipArchiveEntry ze ;
-		byte [] buf = new byte[8 * 1024];
-		String name ;
+	/**
+	 * unzipEntry
+	 *
+	 * @param zis
+	 * @param targetFile
+	 * @return
+	 * @throws IOException
+	 */
+	private static File unzipEntry(ZipArchiveInputStream zis, File targetFile) throws IOException {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(targetFile);
 
-		Stack<File> stack = new Stack<File>();
-		File root ;
-		if ( src.isDirectory() ) {
-			if( includeSrc ){
-				stack.push(src);
-				root = src.getParentFile();
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int len = 0;
+			while ((len = zis.read(buffer)) != -1) {
+				fos.write(buffer, 0, len);
 			}
-			else {
-				File [] fs = src.listFiles();
-				for (int i = 0; i < fs.length; i++) {
-					stack.push(fs[i]);
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (Exception e) {
 				}
-				root = src;
 			}
-		} else {
-			stack.push(src);
-			root = src.getParentFile();
+		}
+		return targetFile;
+	}
+
+	/**
+	 * sourcePath의 파일 OR 디렉토리를 압축한다.
+	 *
+	 * @param sourcePath
+	 *            - 압축 대상 파일 OR 디렉토리
+	 * @param outputFilePath
+	 *            - 저장 zip full path
+	 * @throws Exception
+	 */
+	public static void zip(String sourcePath, String outputFilePath) throws Exception {
+
+		// 압축 대상(sourcePath)이 디렉토리 OR 파일 확인
+		File sourceFile = new File(sourcePath);
+		if (!sourceFile.isFile() && !sourceFile.isDirectory()) {
+			throw new FileNotFoundException("Not exist sourcePath.");
 		}
 
-		while ( !stack.isEmpty() ){
-			File f = stack.pop();
-			name = toPath(root, f);
-			if ( f.isDirectory()){
-				debug ("dir  : " + name);
-				File [] fs = f.listFiles();
-				for (int i = 0; i < fs.length; i++) {
-					if ( fs[i].isDirectory() ) stack.push(fs[i]);
-					else stack.add(0, fs[i]);
+		// 확장자가 Zip 여부 확인
+		if (!(StringUtils.substringAfterLast(outputFilePath, ".")).equalsIgnoreCase("zip")) {
+			throw new Exception("Invalid file suffix.");
+		}
+
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+		ZipArchiveOutputStream zos = null;
+
+		try {
+			fos = new FileOutputStream(outputFilePath);
+			bos = new BufferedOutputStream(fos);
+			zos = new ZipArchiveOutputStream(bos);
+			zos.setLevel(COMPRESSION_LEVEL);
+			zipEntry(sourceFile, sourcePath, zos);
+			zos.finish();
+		} finally {
+			try {
+				if (zos != null)
+					zos.close();
+				if (bos != null)
+					bos.close();
+				if (fos != null)
+					fos.close();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	/**
+	 * 파일 압축
+	 *
+	 * @param sourceFile
+	 * @param sourcePath
+	 * @param zos
+	 * @throws IOException
+	 */
+	private static void zipEntry(File sourceFile, String sourcePath, ZipArchiveOutputStream zos) throws IOException {
+		if (sourceFile.isDirectory()) { // Directory
+			if (sourceFile.getName().equalsIgnoreCase(".metadata")) {
+				return;
+			}
+			File[] fileArray = sourceFile.listFiles();
+			for (int i = 0; i < fileArray.length; i++) {
+				zipEntry(fileArray[i], sourcePath, zos);
+			}
+		} else { // File
+			BufferedInputStream bis = null;
+			try {
+				String sFilePath = sourceFile.getPath();
+				String zipEntryName = sFilePath.substring(sourcePath.length() + 1, sFilePath.length());
+
+				bis = new BufferedInputStream(new FileInputStream(sourceFile));
+				ZipArchiveEntry zentry = new ZipArchiveEntry(zipEntryName);
+				zentry.setTime(sourceFile.lastModified());
+				zos.putArchiveEntry(zentry);
+
+				byte[] buffer = new byte[BUFFER_SIZE];
+				int cnt = 0;
+				while ((cnt = bis.read(buffer, 0, BUFFER_SIZE)) != -1) {
+					zos.write(buffer, 0, cnt);
 				}
-			} else {
-				debug("file : " + name);
-				ze = new ZipArchiveEntry(name);
-				zos.putArchiveEntry(ze);
-				fis = new FileInputStream(f);
-				while ( (length = fis.read(buf, 0, buf.length)) >= 0 ){
-					zos.write(buf, 0, length);
-				}
-				fis.close();
 				zos.closeArchiveEntry();
+			} finally {
+
+				if (bis != null) {
+					bis.close();
+				}
 			}
 		}
-		zos.close();
-	}
-	private String toPath(File root, File dir){
-		String path = dir.getAbsolutePath();
-		path = path.substring(root.getAbsolutePath().length()).replace(File.separatorChar, '/');
-		if ( path.startsWith("/")) path = path.substring(1);
-		if ( dir.isDirectory() && !path.endsWith("/")) path += "/" ;
-		return path ;
-	}
-	private static void debug(String msg){
-		if( debug ) System.out.println(msg);
 	}
 }
