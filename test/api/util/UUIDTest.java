92e995dbc0ad637eccf4cb4adb871c2b67285b8c
이남규 <nklee@ncsoft.com>
2017-04-17 14:57:10 +0900

    UUID 테스트

--- test/api/util/UUIDTest.java
+++ test/api/util/UUIDTest.java
@@ -1,7 +1,9 @@
 package api.util;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
+import com.google.common.io.BaseEncoding;
 import org.junit.Test;
 
 /**
@@ -13,10 +15,18 @@ import org.junit.Test;
  */
 public class UUIDTest {
 
-	@Test
-	public void randomTest() {
-		UUID uuid = UUID.randomUUID();
-		System.out.println(uuid.toString());
-		System.out.println(uuid.version());
-	}
+    @Test
+    public void randomTest() {
+        UUID uuid = UUID.randomUUID();
+        String s = Long.toString(uuid.getMostSignificantBits(), 36) + '-' + Long.toString(uuid.getLeastSignificantBits(), 36);
+        System.out.println(s);
+    }
+
+    @Test
+    public void test() {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid.toString());
+        String idString = Long.toString(uuid.getMostSignificantBits(), 36).replaceAll("-", "").replaceAll("[a-z]", "");
+        System.out.println(idString);
+    }
 }
